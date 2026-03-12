7d0e37b336540aa33afccc026d363cac5b24f676
yunjulanju <127844917+yunjulanju@users.noreply.github.com>
2026-03-12 14:02:04 +0900

    Feat : 수정

--- Main.cpp
+++ Main.cpp
@@ -4,17 +4,17 @@
 
 using namespace std;
 
-int GetRandomNum(int* NumArray)
+int GetRandomNum(int* NumArray , int Size)
 {
-	int RandomNum = rand() % 45 + 1;
-
-	for (int i = 0; i < 45; i++)
+	for (int i = 0; i < Size; i++)
 	{
+		int RandomNum = rand() % Size + 1;
+
 		if (NumArray[RandomNum - 1] == 0)
-			{
-				NumArray[RandomNum - 1] = 1;
-				return RandomNum;
-			}
+		{
+			NumArray[RandomNum - 1] = 1;
+			return RandomNum;
+		}
 	}
 	return -1;
 
@@ -25,19 +25,23 @@ int main()
 	// 1~45
 	srand(static_cast<unsigned int>(std::time(0)));
 
-	//int Number[45];
-	int* Number = new int[45];
-	int Answer[6];
+	//const = 상수 정의
+	int Size = 45;
+	int CollectSize = 6;
 
-	for (int i = 0; i < 45; i++)
+	int* Number = new int[Size];
+	int* Answer = new int[CollectSize];
+
+	for (int i = 0; i < Size; i++)
 	{
 		Number[i] = 0;
 	}
 
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < CollectSize; i++)
 	{
-		Answer[i] = GetRandomNum(Number);
+		Answer[i] = GetRandomNum(Number, Size);
 		cout << Answer[i] << endl;
 	}
+
 	return 0;
 }
