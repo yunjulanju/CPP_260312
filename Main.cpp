0bc21be2bf11cb3a626164880c96f9f954a22a25
yunjulanju <127844917+yunjulanju@users.noreply.github.com>
2026-03-12 09:32:48 +0900

    Feat : 1~45 중 6개 숫자 뽑기

---
+++ Main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <map>
+
+using namespace std;
+
+int GetRandomNum(int* NumArray)
+{
+	int RandomNum = rand() % 45 + 1;
+
+	for (int i = 0; i < 45; i++)
+	{
+		if (NumArray[RandomNum - 1] == 0)
+			{
+				NumArray[RandomNum - 1] = 1;
+				return RandomNum;
+			}
+	}
+	return -1;
+
+}
+
+int main()
+{
+	// 1~45
+	srand(static_cast<unsigned int>(std::time(0)));
+
+	//int Number[45];
+	int* Number = new int[45];
+	int Answer[6];
+
+	for (int i = 0; i < 45; i++)
+	{
+		Number[i] = 0;
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		Answer[i] = GetRandomNum(Number);
+		cout << Answer[i] << endl;
+	}
+
+}
